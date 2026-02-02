722900035b79b1e0b50bbecee6acafd242a73bb0
Baedaar <baedaarrana2895@gmail;com>
2026-02-02 13:37:18 +0100

    initial backend commit

---
+++ src/test/java/esiea/hackathon/leaders/LeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package esiea.hackathon.leaders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
