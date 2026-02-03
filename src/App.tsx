ba9bc281551f06f16050b472a0c4ecdb977904a6
Aymane El Hamraoui <aymane.studies@gmail.com>
2026-02-03 13:57:44 +0100

    feat: core gameplay loop and centralized state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import Lobby from "./pages/Lobby";
 import Game from "./pages/Game";
 
 function App() {
-  return <Game />;
+  const [inGame, setInGame] = useState(false);
+
+  if (inGame) return <Game onBackToLobby={() => setInGame(false)} />;
+  return <Lobby onStartGame={() => setInGame(true)} />;
 }
 
 export default App;
