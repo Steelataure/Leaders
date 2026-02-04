de2ae39b49c6074790307ceef94c1d67e531ba2f
Suban.S <sasikumarsuban2@gmail.com>
2026-02-04 12:03:47 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import Rules from "./pages/Rules.tsx";
-
+import { useState } from "react";
+import Home from "./pages/Home";
+import Lobby from "./pages/Lobby";
+import Game from "./pages/Game";
 
 function App() {
-  return <Rules />;
+  const [step, setStep] = useState("home"); // "home", "lobby", ou "game"
+
+  if (step === "game") {
+    return <Game onBackToLobby={() => setStep("lobby")} />;
+  }
+
+  if (step === "lobby") {
+    return <Lobby onStartGame={() => setStep("game")} />;
+  }
+
+  return <Home onStart={() => setStep("lobby")} />;
 }
 
 export default App;
